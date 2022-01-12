d017e1d0d6f462303d0497107a3ac42e24204a5a
dillonsmithson <dillon.smithson1@gmail.com>
2022-01-12 08:16:35 -0600

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PortfolioWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger){
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string City { get; set; }
+
+        public void OnGet(){
+            // This is what renders the page
+            if (string.IsNullOrWhiteSpace(City))
+                City = "The Web";
+        }
+
+        // Post is what runs when you post data to the page
+    }
+}
